cc43902705fc3fbe91a3395219e080bf23aebaaa
Yurie <yurie.koga.a@gmail.com>
2020-12-16 13:41:06 -0800

    Initial Commit

---
+++ Swift-Assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Assignment3
+//
+//  Created by Uji Saori on 2020-12-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
